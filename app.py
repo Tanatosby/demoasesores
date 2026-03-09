1e55e1f55d37b3c05f5b1c99853d178ceaf14f85
Pedro Gabriel Rotta Saavedra <pedro.rotta.s@udep.edu.pe>
2026-03-09 16:38:56 -0500

    change my db request

--- app.py
+++ app.py
@@ -10,23 +10,35 @@
 from flask import Flask, request, jsonify, session, redirect, render_template, url_for
 import oracledb
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 oracledb.init_oracle_client()
 
+# ─────────────────────────────────────────
+#  CONFIGURACIÓN — Cambia estos valores
+# ─────────────────────────────────────────
+USUARIO  = "tu_usuario"        # Ej: "UDEP_USER"
+PASSWORD = "tu_password"       # Ej: "miPassword123"
+HOST     = "192.168.1.100"     # IP o nombre del servidor Oracle
+PUERTO   = 1521                # Puerto por defecto Oracle
+SERVICIO = "ORCL"              # Nombre del servicio (SID o Service Name)
+# ─────────────────────────────────────────
+
+
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "clave_secreta_cambiar_en_produccion")
+app.secret_key = 'érase una vez un secreto muy secreto'  # Cambia esto por algo seguro en producción
 
 # ─────────────────────────────────────────
 #  Conexión a Oracle
 # ─────────────────────────────────────────
-def get_db():
-    return oracledb.connect(
-        user=os.getenv("DB_USUARIO"),
-        password=os.getenv("DB_PASSWORD"),
-        dsn=f"{os.getenv('DB_HOST')}:{os.getenv('DB_PUERTO', 1521)}/{os.getenv('DB_SERVICIO')}"
-    )
+#def get_db():
+  #  dsn = f"{HOST}:{PUERTO}/{SERVICIO}"
+  #  return oracledb.connect(
+   #     user=USUARIO,
+  #      password=PASSWORD,
+   #     dsn=dsn
+   # )
 
 # ─────────────────────────────────────────
 #  Rutas de páginas
@@ -62,7 +74,15 @@ def login():
 
     # Buscar PIDM en Oracle con el correo ingresado
     try:
-        conn   = get_db()
+        dsn = f"{HOST}:{PUERTO}/{SERVICIO}"
+        conn = oracledb.connect(
+            user=USUARIO,
+            password=PASSWORD,
+            dsn=dsn
+         )
+        print("✅ Conexión exitosa!")
+        print(f"   Versión Oracle: {conn.version}\n")
+      # conn   = get_db()
         cursor = conn.cursor()
         cursor.execute("""
             SELECT MAL.GOREMAL_PIDM FROM GENERAL.GOREMAL MAL WHERE MAL.GOREMAL_PREFERRED_IND = 'Y' AND MAL.GOREMAL_EMAIL_ADDRESS = :correo
